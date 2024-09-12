877673b6e7d6055243d23a5b757b9bcdf186d747
IsaacAHS <isaac.huamani@pucp.edu.pe>
2024-09-11 20:33:50 -0500

    owo

---
+++ lab3_20215421/src/main/java/entity/Receta.java
@@ -0,0 +1,12 @@
+package entity;
+
+import com.fasterxml.jackson.annotation.JsonAnyGetter;
+import com.fasterxml.jackson.annotation.JsonAnySetter;
+import jakarta.persistance.*;
+import lombok.Getter;
+import lombok.Getter;
+
+@Getter
+@Setter
+public class Receta {
+}
