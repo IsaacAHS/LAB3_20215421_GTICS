10b4e0d9c7685fe1391e3d182350a4fca76430cf
IsaacAHS <142356395+IsaacAHS@users.noreply.github.com>
2024-09-11 20:12:57 -0500

    Add files via upload

---
+++ LAB3_20215421/LAB3_20215421/src/main/java/com/example/lab3_20215421/Lab320215421Application.java
@@ -0,0 +1,13 @@
+package com.example.lab3_20215421;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab320215421Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab320215421Application.class, args);
+    }
+
+}
---
+++ LAB3_20215421/LAB3_20215421/src/test/java/com/example/lab3_20215421/Lab320215421ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lab3_20215421;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab320215421ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
